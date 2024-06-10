e96f44403709b2f176939b886c32ba870c41a090
Whiskerss <abhishekjas2001@gmai.com>
2024-06-10 20:31:33 +0545

    getting data from mock api

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import '../services/auth_service.dart';
 import '../widgets/cstm_appbar.dart';
 
@@ -12,12 +13,49 @@ class MenuPage extends StatefulWidget {
 
 class _MenuPageState extends State<MenuPage> {
   final AuthService authService = AuthService();
-
-  List categories = ['All', 'Starter', 'Main Course', 'Drinks', 'Dessert'];
+  List<String> categories = [
+    'All',
+    'Starter',
+    'Main Course',
+    'Drinks',
+    'Dessert'
+  ];
+  String selectedCategory = 'All';
+  List<MenuItem> menuItems = [];
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
+    fetchMenuItems();
+  }
+
+  Future<void> fetchMenuItems() async {
+    try {
+      final response = await http.get(
+          Uri.parse('https://66670cb7a2f8516ff7a6169f.mockapi.io/restro/menu'));
+      if (response.statusCode == 200) {
+        List jsonResponse = json.decode(response.body);
+        setState(() {
+          menuItems =
+              jsonResponse.map((item) => MenuItem.fromJson(item)).toList();
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load menu items');
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      print(e);
+    }
+  }
+
+  void selectCategory(String category) {
+    setState(() {
+      selectedCategory = category;
+    });
   }
 
   @override
@@ -27,70 +65,101 @@ class _MenuPageState extends State<MenuPage> {
         preferredSize: Size.fromHeight(80),
         child: CstmAppBar(),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              color: const Color(0xFFE0DDDD),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(
-                    height: 60,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: categories.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        String category = categories[index];
-                        return Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              padding: const EdgeInsets.all(10),
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                color: const Color(0xFFFFAD39),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              child: Text(
-                                category.toUpperCase(),
-                                style: const TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.white),
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
+                  CategoryList(
+                    categories: categories,
+                    selectedCategory: selectedCategory,
+                    onCategorySelect: selectCategory,
                   ),
+                  MenuGrid(menuItems: menuItems),
                 ],
               ),
             ),
-            GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 4,
-                  mainAxisSpacing: 4,
-                  mainAxisExtent: 250),
-              itemBuilder: (_, index) => const MenuCard(),
-              itemCount: 5,
-            )
-          ],
-        ),
+    );
+  }
+}
+
+class CategoryList extends StatelessWidget {
+  final List<String> categories;
+  final String selectedCategory;
+  final ValueChanged<String> onCategorySelect;
+
+  const CategoryList({
+    Key? key,
+    required this.categories,
+    required this.selectedCategory,
+    required this.onCategorySelect,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFFE0DDDD),
+      height: 60,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index) {
+          String category = categories[index];
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: GestureDetector(
+              onTap: () => onCategorySelect(category),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: selectedCategory == category
+                      ? const Color(0xFFFFAD39)
+                      : const Color(0xFFC0C0C0),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  category.toUpperCase(),
+                  style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
 
+class MenuGrid extends StatelessWidget {
+  final List<MenuItem> menuItems;
+
+  const MenuGrid({Key? key, required this.menuItems}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 4,
+        mainAxisSpacing: 4,
+        mainAxisExtent: 250,
+      ),
+      itemBuilder: (_, index) => MenuCard(menuItem: menuItems[index]),
+      itemCount: menuItems.length,
+    );
+  }
+}
+
 class MenuCard extends StatelessWidget {
-  const MenuCard({
-    super.key,
-  });
+  final MenuItem menuItem;
+
+  const MenuCard({super.key, required this.menuItem});
 
   @override
   Widget build(BuildContext context) {
@@ -99,12 +168,20 @@ class MenuCard extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Image.network(
-            'https://www.nrn.com/sites/nrn.com/files/styles/article_featured_standard/public/Chick-fil-A%20Pizza%20Pie.jpg?itok=9pU42iU0',
+            menuItem.imageUrl,
             height: 100,
           ),
-          const ListTile(
-            title: Text('Pizza'),
-            subtitle: Text('Category: Mamma Mia'),
+          ListTile(
+            title: Text(
+              menuItem.title,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            subtitle: Text(
+              'Category: ${menuItem.category}',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
           ),
           ButtonBar(
             alignment: MainAxisAlignment.spaceBetween,
@@ -129,6 +206,26 @@ class MenuCard extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class MenuItem {
+  final String title;
+  final String category;
+  final String imageUrl;
+
+  MenuItem({
+    required this.title,
+    required this.category,
+    required this.imageUrl,
+  });
+
+  factory MenuItem.fromJson(Map<String, dynamic> json) {
+    return MenuItem(
+      title: json['title'],
+      category: json['category'],
+      imageUrl: json['imageUrl'],
     );
   }
 }
--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/cstm_appbar.dart';
+
 class OrderPage extends StatefulWidget {
   const OrderPage({super.key});
 
@@ -10,6 +12,66 @@ class OrderPage extends StatefulWidget {
 class _OrderPageState extends State<OrderPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text('This is Order Page');
+    return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: CstmAppBar(),
+      ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        itemBuilder: (_, index) => const OrderCard(),
+        itemCount: 5,
+      ),
+    );
+  }
+}
+
+class OrderCard extends StatelessWidget {
+  const OrderCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            title: const Text('Pizza'),
+            subtitle: const Text('Category: Mamma Mia'),
+            trailing: Image.network(
+              'https://www.nrn.com/sites/nrn.com/files/styles/article_featured_standard/public/Chick-fil-A%20Pizza%20Pie.jpg?itok=9pU42iU0',
+              height: 100,
+            ),
+          ),
+          Image.network(
+            'https://www.nrn.com/sites/nrn.com/files/styles/article_featured_standard/public/Chick-fil-A%20Pizza%20Pie.jpg?itok=9pU42iU0',
+            height: 100,
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextButton(
+                onPressed: () {
+                  // Perform some action
+                },
+                child: const Icon(Icons.add),
+              ),
+              const Text(
+                '4',
+                textScaleFactor: 2,
+              ),
+              TextButton(
+                onPressed: () {
+                  // Perform some action
+                },
+                child: const Icon(Icons.remove),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
