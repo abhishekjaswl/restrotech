a71e9ea51ffc65e0dd1fa6a44fa4a3ee0133dcfa
Whiskerss <abhishekjas2001@gmai.com>
2024-06-19 13:17:54 +0545

    order card

--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -1,3 +1,4 @@
+import 'package:avatars/avatars.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/cstm_appbar.dart';
@@ -38,36 +39,109 @@ class OrderCard extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           ListTile(
-            title: const Text('Pizza'),
-            subtitle: const Text('Category: Mamma Mia'),
-            trailing: Image.network(
-              'https://www.nrn.com/sites/nrn.com/files/styles/article_featured_standard/public/Chick-fil-A%20Pizza%20Pie.jpg?itok=9pU42iU0',
-              height: 100,
+            title: const Text('Order #69420'),
+            subtitle: const Text(
+              '05 Feb 2023, 08:28 PM',
+              style: TextStyle(
+                color: Colors.black38,
+              ),
+            ),
+            trailing: Avatar(
+              sources: [
+                if (true)
+                  NetworkSource(
+                      'https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg')
+              ],
+              shape: AvatarShape.circle(25),
+              name: 'user name',
             ),
           ),
-          Image.network(
-            'https://www.nrn.com/sites/nrn.com/files/styles/article_featured_standard/public/Chick-fil-A%20Pizza%20Pie.jpg?itok=9pU42iU0',
-            height: 100,
+          ListTile(
+            title: const Text('Vegetable Mixups'),
+            subtitle: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Vegetable Fritters with Egg',
+                  style: TextStyle(color: Colors.black38, fontSize: 15),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '520',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                    Text(
+                      'Qty: 1',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            leading: Avatar(
+              sources: [
+                if (true)
+                  NetworkSource(
+                      'https://www.nrn.com/sites/nrn.com/files/styles/article_featured_standard/public/Chick-fil-A%20Pizza%20Pie.jpg?itok=9pU42iU0')
+              ],
+              border: Border.all(
+                color: Theme.of(context).colorScheme.primary,
+                width: 1,
+              ),
+              shape: AvatarShape.circle(50),
+              name: 'food name',
+            ),
+          ),
+          const Divider(
+            indent: 20,
+            endIndent: 20,
           ),
           ButtonBar(
             alignment: MainAxisAlignment.spaceBetween,
             children: [
-              TextButton(
-                onPressed: () {
-                  // Perform some action
-                },
-                child: const Icon(Icons.add),
-              ),
               const Text(
-                '4',
-                textScaleFactor: 2,
+                'X2 Items',
+                style: TextStyle(color: Colors.black38, fontSize: 15),
               ),
-              TextButton(
-                onPressed: () {
-                  // Perform some action
-                },
-                child: const Icon(Icons.remove),
-              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 2.0),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: IconButton(
+                      icon: const Icon(Icons.close, color: Colors.red),
+                      onPressed: () {
+                        // Perform some action
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green, width: 2.0),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                      onPressed: () {
+                        // Perform some action
+                      },
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ],
